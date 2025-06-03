a52f781171d8acafd0faabfac2856d6c7aac049b
F4rewall <tremtremdazueira@gmail.com>
2025-06-03 19:29:17 -0300

    Atividade De Pasta

---
+++ comitis/atividadepasta00.py
@@ -0,0 +1,16 @@
+import os
+
+lista_arquivos = os.listdir("documentos")
+
+print(lista_arquivos)
+
+for arquivo in lista_arquivos:
+    if '.doc' in arquivo or '.docx' in arquivo:
+        os.rename(f"documentos/{arquivo}", f"documentos/word/{arquivo}")
+    
+    elif '.xls' in arquivo or '.xlsx' in arquivo:
+        os.rename(f"documentos/{arquivo}", f"documentos/excel/{arquivo}")
+    
+    elif '.pptx' in arquivo or '.pptx' in arquivo:
+        os.rename(f"documentos/{arquivo}", f"documentos/powerpoint/{arquivo}")
+#------------------------------------------------------------------------------
